a6fe13903f7fcf69b4bf5cd177e3e959aa1c1719
davidlilili <liboxilife@gmail.com>
2019-12-08 03:08:07 +0000

    Add update

--- squirrel/urls.py
+++ squirrel/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 app_name='squirrel'
 urlpatterns=[
+    path('', views.all_squirrels),
     path('map/',views.map,name='map'),
 ]
--- squirrel/views.py
+++ squirrel/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Squirrel
+from django.http import HttpResponse
 
 def map(request):
     squirrels=Squirrel.objects.all()[:50]
@@ -7,3 +8,9 @@ def map(request):
         'squirrels':squirrels,
     }
     return render(request, 'squirrel/map.html', context)
+
+def all_squirrels(request):
+    squirrel_list = Squirrel.objects.all()[:5]
+    context = {'squirrel_list': squirrel_list}
+    return render(request, 'squirrel/index.html', context)
+
